df5c22bdc711a1abb3e426a92fedd5c2298e5103
Shah-Shishir <shahshishir.09@gmail.com>
2023-01-19 02:02:55 +0600

    add helpers for image generation

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -3,16 +3,37 @@ const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
 
+// Helper Methods
+const { validateImageGeneration } = require("../utils/openaiHelpers");
+
 const generateImages = async (req, res) => {
     let { prompt, n, size } = req.body;
 
-    if (!prompt || !prompt?.length) {
-        res.status(400).json({ message: 'Image description is empty.' });
+    const respose = validateImageGeneration(prompt, n, size);
+
+    if (respose.errors) {
+        res.status(400).json({ message: respose.message });
+    } else {
+        prompt = respose.prompt;
+        n = respose.n;
+        size = respose.size;
     }
 
-    if (prompt.length > 1000) {
-        res.status(400).json({ message: 'Image description can not have more than 1000 characters.' });
+    try {
+        const response = await openai.createImage({
+            prompt,
+            n,
+            size
+        });
+
+        res.status(200).json({ data: response.data.data, message: 'Image generation is successful.' });
+    } catch (err) {
+        res.status(400).json({ message: 'Unable to generate desired image(s).' });
     }
+};
+
+const generateImageVariations = async (req, res) => {
+    let { image, n, size } = req.body;
 
     if (n > 10) {
         res.status(400).json({ message: 'Maximum number of images exceeded.' });
@@ -33,7 +54,7 @@ const generateImages = async (req, res) => {
     size = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
 
     try {
-        const response = await openai.createImage({
+        const response = await openai.createImageVariation({
             prompt,
             n,
             size
---
+++ utils/openaiHelpers.js
@@ -0,0 +1,50 @@
+const validateImageGeneration = (prompt, n, size) => {
+    if (!prompt || !prompt?.length) {
+        return ({ 
+            errors: true,
+            message: 'Image description is empty.' 
+        });
+    }
+
+    if (prompt.length > 1000) {
+        return ({ 
+            errors: true,
+            message: 'Image description can not have more than 1000 characters.' 
+        });
+    }
+
+    if (n > 10) {
+        return ({ 
+            errors: true,
+            message: 'Maximum number of images exceeded.' 
+        });
+    }
+
+    if (!['small', 'medium', 'large'].includes(size)) {
+        return ({ 
+            errors: true,
+            message: 'Invalid size.' 
+        });
+    }
+
+    if (!n) {
+        n = 1;
+    }
+
+    if (!size || !size?.length) {
+        size = '1024x1024';
+    }
+
+    size = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+
+    return ({ 
+        errors: false,
+        prompt,
+        n,
+        size 
+    });
+};
+
+module.exports = {
+    validateImageGeneration
+};
